c7093cbaa7be2e3fccdc52dca4a9e96af7af610b
Brennan <bjg813@gmail.com>
2019-05-08 21:45:40 -0700

    changed title to sacramento web developer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `BulbousBow`,
+    title: `Sacramento Web Developer`,
     author: `Brennan`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
